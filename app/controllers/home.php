3bf9f410964c2cf7eb67ab19a12ecf2289f598af
mattperkins <github@mattperkins.com>
2019-04-23 19:51:49 +0100

    init

---
+++ app/controllers/home.php
@@ -0,0 +1,15 @@
+<?php 
+
+class Home extends Controller
+{
+  public function index($id="")
+  {
+    $user = $this->model("User");
+    $user->id = $id;
+
+    $this->view("home/index", ["id" => $user->id]);
+  }
+
+  
+
+}
