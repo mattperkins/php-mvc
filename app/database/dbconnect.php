199b266727e753d7ca021fb1900b24e728890454
mattperkins <github@mattperkins.com>
2019-04-24 16:16:05 +0100

    db connect config

--- app/database/dbconnect.php
+++ app/database/dbconnect.php
@@ -19,6 +19,7 @@ $capsule->addConnection([
     'prefix'    => '',
 ]);
 
+
 // Set the event dispatcher used by Eloquent models... (optional)
 use Illuminate\Events\Dispatcher;
 use Illuminate\Container\Container;
@@ -26,6 +27,8 @@ $capsule->setEventDispatcher(new Dispatcher(new Container));
 
 // Make this Capsule instance available globally via static methods... (optional)
 $capsule->setAsGlobal();
+
+
 
 // Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
 $capsule->bootEloquent();
