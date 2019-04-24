b0e3437ebda6202a2dd03f0365b8d00184e8229b
mattperkins <github@mattperkins.com>
2019-04-24 23:04:02 +0100

    scaffold

---
+++ app/views/partials/nav.php
@@ -0,0 +1,22 @@
+<header class="mb-5">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">PHP-MVC</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/index">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/add-dept">Add Department</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/add-subj">Add Subject</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</header>
