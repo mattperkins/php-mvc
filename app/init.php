3fb59eb7ee0db56f8390c11af4b66cdd35098e17
mattperkins <github@mattperkins.com>
2019-04-24 16:16:49 +0100

    add autoload

--- app/init.php
+++ app/init.php
@@ -1,4 +1,8 @@
 <?php 
 
+// composer autoloader
+require_once("../vendor/autoload.php");
+
+// require_once("database/dbconnect.php");
 require_once("core/App.php");
 require_once("core/Controller.php");
