ee034fe1762ebb3e9c9a68ad43b9e5f02cd2bf64
mattperkins <github@mattperkins.com>
2019-04-25 16:03:48 +0100

    add form UI and data configuration

--- app/views/add-dept/index.php
+++ app/views/add-dept/index.php
@@ -1,10 +1,36 @@
 <?php 
+// require '../vendor/autoload.php';
+$message = "";
+if(isset($_POST["name"]))
+{
+  Department::insert([
+    "name" => $_POST["name"]
+  ]);
+  $message = "Data inserted successfully";
+}
 ob_start();
 ?>
 
-<div>
-  Add Department
+<!-- ignored because route redirects to index -->
+<div class="message">
+  <?php if(!empty($message)): ?>
+    <div class="alert alert-success">
+      <?= $message ?>
+    </div>
+  <?php endif; ?>
 </div>
+<!-- \ ignored because route redirects to index -->
+
+
+<form action="" method="post">
+  <div class="form-group">
+    <label for="name">Department Name</label>
+    <input class="form-control" type="text" name="name" id="name">
+  </div>
+  <div class="form-group">
+    <button class="btn btn-outline-info" type="submit">Add a department</button>
+  </div>
+</form>
 
 <?php 
 $content = ob_get_clean();
