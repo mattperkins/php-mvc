8f8ed664460b3c714c13295e2c3cea2073a07b5f
mattperkins <github@mattperkins.com>
2019-04-25 16:04:30 +0100

    update brand link

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,10 +1,18 @@
 <?php 
+$departments = Department::all();
 ob_start();
 ?>
 
-<div>
-  home
-</div>
+<h2>All Departments</h2>
+<?php foreach($departments as $department): ?>
+  <h4><?= $department->name ?></h4>
+  <?php foreach($department->subjects as $subject): ?>
+    <li class="list-group mb-2">
+      <?= $subject->name ?>
+    </li>
+  <?php endforeach ?>
+<?php endforeach ?>
+
 
 <?php 
 $content = ob_get_clean();
