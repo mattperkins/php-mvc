664adc3a6146fd7d9da0ce0d0f275c90644d11b7
mattperkins <github@mattperkins.com>
2019-04-25 16:04:02 +0100

    add form UI and data configuration

--- app/views/add-subj/index.php
+++ app/views/add-subj/index.php
@@ -1,10 +1,47 @@
-<?php 
+<?php
+$departments = Department::all(); 
+$message = "";
+if(isset($_POST["name"]))
+{
+  Subject::insert([
+    "name" => $_POST["name"],
+    "department_id" => $_POST["department"]
+  ]);
+  $message = "Data inserted successfully";
+}
 ob_start();
 ?>
 
-<div>
-  Add Subject
+<!-- ignored because route redirects to index -->
+<div class="message">
+  <?php if(!empty($message)): ?>
+    <div class="alert alert-success">
+      <?= $message ?>
+    </div>
+  <?php endif; ?>
 </div>
+<!-- \ ignored because route redirects to index -->
+
+
+<form action="" method="post">
+  <div class="form-group">
+    <label for="name">Subject Name</label>
+    <input class="form-control" type="text" name="name" id="name">
+  </div>
+  <div class="form-group">
+    <label for="department">Department</label>
+    <select name="department" class="form-control" id="department">
+      <?php foreach($departments as $department): ?>
+        <option value="<?= $department->id ?>">
+          <?= $department->name ?>
+        </option>
+      <?php endforeach ?>
+    </select>
+  </div>
+  <div class="form-group">
+    <button class="btn btn-outline-info" type="submit">Add a subject</button>
+  </div>
+</form>
 
 <?php 
 $content = ob_get_clean();
