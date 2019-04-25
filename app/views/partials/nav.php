aa542d1d120746ec7db4f36ec9900927fc690c7e
mattperkins <github@mattperkins.com>
2019-04-25 16:06:15 +0100

    update brand link

--- app/views/partials/nav.php
+++ app/views/partials/nav.php
@@ -1,6 +1,6 @@
 <header class="mb-5">
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">PHP-MVC</a>
+  <a class="navbar-brand" href="/">PHP-MVC</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -8,7 +8,7 @@
     
     <ul class="navbar-nav ml-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="/index">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="/add-dept">Add Department</a>
